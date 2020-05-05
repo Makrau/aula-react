6a2751ed907ad6c5cabfbc831bc868b040d39da4
Iago Goncalves <iago.goncalves@klickpages.com.br>
2020-05-05 13:31:28 -0300

    criada estilizacao inicial da aplicacao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/7682908?s=460&u=6b102a76731c66e0ac01ac085e0109cb9ff75f47&v=4"
+            alt="Makrau"
+          />
+          <div>
+            <strong>Nome do repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/7682908?s=460&u=6b102a76731c66e0ac01ac085e0109cb9ff75f47&v=4"
+            alt="Makrau"
+          />
+          <div>
+            <strong>Nome do repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/7682908?s=460&u=6b102a76731c66e0ac01ac085e0109cb9ff75f47&v=4"
+            alt="Makrau"
+          />
+          <div>
+            <strong>Nome do repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
